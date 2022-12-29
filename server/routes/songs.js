23bf6d45571b6799a3a86ab405a0e941b8d8d556
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2022-12-29 20:35:17 +0100

    GET endpoint to get all songs and POST endpoint to add a new song to the playlist. Both are tested in REST Client

--- server/routes/songs.js
+++ server/routes/songs.js
@@ -2,8 +2,39 @@ import express from 'express';
 
 const router = express.Router();
 
+const songs = [
+    {
+        title: "Jul Igen",
+        artist: "JustD",
+        genre: "Christmas",
+        id: 1
+    },
+    {
+        title: "Sommaren Är Kort",
+        artist: "Thomas Ledin",
+        genre: "Sommar",
+        id: 2
+    }
+]
+
 router.get('/', (req, res) => {
-    res.send("Hello");
+    console.log(songs);
+
+    res.send(songs);
 });
+
+
+//Vi skickar data från frontend till servern
+router.post('/', (req, res) => {
+    console.log("POST route reached");
+
+    const newSong = req.body;
+
+    songs.push(newSong);
+
+    res.send(`${newSong.title} is added to the playlist`);
+});
+
+
 
 export default router;
