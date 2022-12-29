baae48eda15dadb42dc48ffaf9dc3c4bceccf0a2
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2022-12-29 20:06:30 +0100

    create a file with name routes, and create mock-up for the songs in json-file

--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 import express from 'express'; // = const express = require("express");
 import bodyParser from 'body-parser'; //tillåter oss att ta emot data från body
 
+import songsRoutes from './routes/songs.js';
+
 const app = express();
 const PORT = 3000;
 
 app.use(bodyParser.json()); //vi kan använda json-data
+app.use('/songs', songsRoutes);
 
 //First route
 app.get('/', (req, res) => res.send("Hello from first route"));
