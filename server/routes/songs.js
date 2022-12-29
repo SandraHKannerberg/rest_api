9a5f6ec989fd122c0b0d7fbf0ecc21da3f841299
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2022-12-29 21:20:35 +0100

    create GET, GET/:id, POST and DELETE endpoints

--- server/routes/songs.js
+++ server/routes/songs.js
@@ -1,22 +1,11 @@
 import express from 'express';
+import { v4 as uuidv4 } from 'uuid'; //Ger unikt id till varje låt
 
 const router = express.Router();
 
-const songs = [
-    {
-        title: "Jul Igen",
-        artist: "JustD",
-        genre: "Christmas",
-        id: 1
-    },
-    {
-        title: "Sommaren Är Kort",
-        artist: "Thomas Ledin",
-        genre: "Sommar",
-        id: 2
-    }
-]
+let songs = [];
 
+//GET endpoint för hela spellistan
 router.get('/', (req, res) => {
     console.log(songs);
 
@@ -24,17 +13,35 @@ router.get('/', (req, res) => {
 });
 
 
-//Vi skickar data från frontend till servern
+//POST endpoint för att lägga till en låt till spellistan (Vi skickar data från frontend till servern)
 router.post('/', (req, res) => {
-    console.log("POST route reached");
-
+   
     const newSong = req.body;
 
-    songs.push(newSong);
+    songs.push({ ...newSong, id: uuidv4()});
 
     res.send(`${newSong.title} is added to the playlist`);
 });
 
+//GET endpoint med id för att få fram en specifik låt
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+
+    const foundSong = songs.find((song) => song.id === id);
+
+    res.send(foundSong);
+});
+
+//DELETE endpoint för att radera en låt från spellistan
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+
+    songs = songs.filter((song) => song.id !== id);
+
+    res.send("Deleted from playlist");
+});
+
+//PUT endpoint
 
 
 export default router;
