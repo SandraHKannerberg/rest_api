ab4b5aad4ca7e801c2040ed0c4e9f6788ab561bf
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2023-01-03 16:08:38 +0100

    start create css for the playlist

--- client/songs.js
+++ client/songs.js
@@ -17,28 +17,50 @@ function getDataWithThen(){
             songContainer.classList.add("songContainer");
             playlistContainer.appendChild(songContainer);
 
-            //SKICKAR IN LÅTENS TITEL
-            const title = document.createElement("h2")
+            //CONTAINER FÖR TEXTEN I VARJE "LÅTBOX"
+            const textContainer = document.createElement("div")
+            textContainer.classList.add("textContainer");
+            songContainer.appendChild(textContainer);
+
+            //LÅTENS TITEL
+            const title = document.createElement("h3")
             title.classList.add("songTitle");
             title.textContent = song.title;
 
-            //SKICKAR IN ARTISTEN
+            //ARTISTEN
             const artist = document.createElement("h3")
             artist.classList.add("artist");
             artist.textContent = song.artist;
 
-            //SKICKAR IN LÅTENS GENRE
-            const genre = document.createElement("p")
+            //LÅTENS GENRE
+            const genre = document.createElement("h3")
             genre.classList.add("genre");
             genre.textContent = song.genre;
 
-            //VARJE LÅTS INFO LÄGGS TILL I EN EGEN BOX
-            songContainer.appendChild(title)
-            songContainer.appendChild(artist)
-            songContainer.appendChild(genre)
+            //VARJE LÅTS INFO LÄGGS TILL I TEXTCONTAINERN
+            textContainer.appendChild(title)
+            textContainer.appendChild(artist)
+            textContainer.appendChild(genre)
 
             //LÅTBOXARNA LÄGGS TILL I PLAYLISTCONTAINERN
             playlistContainer.appendChild(songContainer);
+
+            //CONTAINER FÖR KNAPPARNA
+            const btnContainer = document.createElement("div")
+            btnContainer.classList.add("btnContainer");
+            songContainer.appendChild(btnContainer);
+
+            //KNAPP FÖR ATT UPPDATERA SPELLISTAN
+            const updateBtn = document.createElement("button"); 
+            updateBtn.classList.add("updateBtn");
+            updateBtn.innerText = "Uppdatera";
+            btnContainer.appendChild(updateBtn);
+
+            //KNAPP FÖR ATT RADERA EN LÅT FRÅN SPELLISTAN
+            const delBtn = document.createElement("button"); 
+            delBtn.classList.add("delBtn");
+            delBtn.innerText = "Delete";
+            btnContainer.appendChild(delBtn);
         };
     })
     .catch(function (err) {
