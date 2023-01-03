8eb9566f4bc01a81e7fd7a2ffc7e99c5c34c899b
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2023-01-03 14:37:57 +0100

    Create getDataWithThen-function

--- client/songs.js
+++ client/songs.js
@@ -1,78 +1,8 @@
-console.log("Hello World!");
-
 const container = document.querySelector(".container");
 const playlistContainer = document.createElement("div");
 playlistContainer.classList.add("playlistContainer");
 
 container.appendChild(playlistContainer);
-
-
-
-
-/*const renderHeader = () => {
-    //Kontrollera varför inte denna del fungerar
-    const header = document.createElement("div");
-    header.classList.add = ("header");
-    rootElement.appendChild(header);
-
-    const headerText = document.createElement("h1");
-    headerText.classList.add = ("header-text");
-    headerText.textContent = "Playlist";
-    header.appendChild(headerText);
-}
-
-renderHeader();*/
-
-/*const getSongs = () => {
-    fetch("http://localhost:3000/songs").then(response => response.json()).then(data => {
-        const songsContainer = createSongsContainer();
-
-
-        data.foreach(song => {
-            const songNode = createSong(song);
-            songsContainer.appendChild(songNode);
-        })
-        
-        rootElement.appendChild(songsContainer);
-    })
-}
-
-const createSongsContainer = () => {
-    const container = document.createElement("div")
-    container.classList.add("songs-container")
-
-    return container;
-}
-
-const createSong = (songData) => {
-    const song = document.createElement("div")
-
-    const title = document.createElement("h2")
-    title.classList.add("song-title");
-    title.textContent = songData.title;
-
-    const artist = document.createElement("h3")
-    artist.classList.add("artist");
-    artist.textContent = songData.artist;
-
-    const genre = document.createElement("p")
-    genre.classList.add("genre");
-    genre.textContent = songData.genre;
-
-    const innerContainer = document.createElement("div")
-    innerContainer.classList.add("song-inner-container")
-
-    innerContainer.appendChild(title)
-    innerContainer.appendChild(artist)
-    innerContainer.appendChild(genre)
-
-    song.appendChild(innerContainer)
-
-    return song;
-}
-
-
-getSongs();*/
 
 function getDataWithThen(){
     fetch("http://localhost:3000/songs")
@@ -80,7 +10,36 @@ function getDataWithThen(){
         return response.json();
     })
     .then(function (data) {
-        console.log(data);
+
+        for (const song of data) {
+            //SKAPAR VARJE "LÅTBOX"
+            const songContainer = document.createElement("div")
+            songContainer.classList.add("songContainer");
+            playlistContainer.appendChild(songContainer);
+
+            //SKICKAR IN LÅTENS TITEL
+            const title = document.createElement("h2")
+            title.classList.add("songTitle");
+            title.textContent = song.title;
+
+            //SKICKAR IN ARTISTEN
+            const artist = document.createElement("h3")
+            artist.classList.add("artist");
+            artist.textContent = song.artist;
+
+            //SKICKAR IN LÅTENS GENRE
+            const genre = document.createElement("p")
+            genre.classList.add("genre");
+            genre.textContent = song.genre;
+
+            //VARJE LÅTS INFO LÄGGS TILL I EN EGEN BOX
+            songContainer.appendChild(title)
+            songContainer.appendChild(artist)
+            songContainer.appendChild(genre)
+
+            //LÅTBOXARNA LÄGGS TILL I PLAYLISTCONTAINERN
+            playlistContainer.appendChild(songContainer);
+        };
     })
     .catch(function (err) {
         console.log(err);
